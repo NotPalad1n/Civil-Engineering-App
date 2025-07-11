321df942d8dad7dff6ea15302ce0c2882c1c1f72
Younes Oumast <younesoumast2@gmail.com>
2025-07-11 01:52:54 +0100

    Modified Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,8 +3,18 @@ import Link from 'next/link';
 export default function Hero() {
   return (
     <div className="z-10 flex flex-col items-center justify-center h-full text-center px-4 py-50">
-      <h1 className="text-4xl sm:text-5xl font-bold mb-4">
-        Des outils de génie civil simples et efficaces
+      <h1 className="text-4xl sm:text-5xl font-bold mb-8">
+        {/* Des outils de génie civil simples et efficaces */}
+        Des outils de génie civil&nbsp;
+        <span className="relative inline-block">
+          <span className="relative z-10">simples</span>
+          <span className="absolute -bottom-2 left-0 w-full h-2 bg-blue-600 rounded-full z-0"></span>
+        </span>
+        &nbsp;et&nbsp;
+        <span className="relative inline-block">
+          <span className="relative z-10">efficaces</span>
+          <span className="absolute -bottom-2 left-0 w-full h-2 bg-blue-600 rounded-full z-0"></span>
+        </span>
       </h1>
       <p className="text-lg sm:text-xl text-black mb-8">
         Calculez les sections de ferraillage pour les poteaux, poutres, dalles et semelles.
